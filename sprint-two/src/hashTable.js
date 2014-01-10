b4f8dc0f2c5dd4ad5d2aacc81f95f82bebdf93b9
felixcheng <chengfelix@gmail.com>
2014-01-10 14:46:52 -0800

    All of section two

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,36 @@
 var HashTable = function(){
   this._limit = 8;
-  this._storage = makeLimitedArray(this._limit);
+  this.counter = 0;
+  this.container = makeLimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i] = v;
+  this.container.set(i, v);
+  // console.log('bucket', this.bucket);
+  this.container[i] = v;
+  this.counter++;
+
+  if(this.counter / this._limit >= 0.75) {
+    this._limit *= 2;
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage[i];
+  return this.container[i];
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i] = null;
+  console.log('limit',this.limit, this._limit);
+  this.container.set(i, null);
+  this.container[i] = null;
+  this.counter--;
+
+  if(this.counter / this._limit < 0.25) {
+    this._limit /= 2;
+  // console.log(this._storage)
+  }
 };
 
